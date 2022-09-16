dd78cdc9e5e7c344f69e232944a30fed69945b03
Sombre-all <111535816+Sombre-all@users.noreply.github.com>
2022-09-16 21:47:18 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,25 +7,25 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['2250585202312']
+global.OwnerNumber = ['2250585202312']
+global.ownertag = ['2250585202312']
+global.BotName = "MIKU BOT SOMBRE"
+global.packname = "SOMBRE • DAEMON/🕊️"
+global.author = "By: SOMBRE DAEMON"
+global.OwnerName = "SOMBRE DAEMON"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
 
 // Prefix //
 // If you want to change the prefix, change it and also change all switch's button id from Core.js  otherwise buttons will not work //
-global.prefa = ['-'] 
+global.prefa = [','] 
 
 
 global.location = "West Bengal, India"
-global.reactmoji = "❤️"
-global.themeemoji = "💖"
+global.reactmoji = "🕊️"
+global.themeemoji = "🥷"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
 global.websitex = "https://github.com/FantoX001"
 global.lolhuman = "KaysaS"
